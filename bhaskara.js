88e0e6cf88de6dd48ad2fc9b0e16f3b1b4dde815
bmelovaz <biamelovaz@gmail.com>
2023-11-30 14:11:40 -0300

    Calculo de Bhaskara

--- bhaskara.js
+++ bhaskara.js
@@ -1,5 +1,21 @@
 const readline = require('readline')
 
+function calcularBhaskara(a, b, c){
+    const delta = b ** 2 - 4 * a * c
+
+    if (delta < 0){
+        console.log('A equação não possui raiz reais')
+    } else if (delta === 0){
+        const x = - b / (2*a)
+        console.log('A equação possui uma raiz real')
+    } else {
+        const x1 = (- b + Math.sqrt(delta)) / (2 * a)
+        const x2 = (- b + Math.sqrt(delta)) / (2 * a)
+        console.log('A equação possui duas raizes reais')
+    } 
+
+}
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
